6cc2e323b0aab27114aa94ac6e3ecd9c6b1b0d2f
Denis00-ES <dstoynov2000@gmail.com>
2025-05-21 23:44:51 +0300

    move calculator header to separate component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,4 +6,6 @@ import { Component } from "@angular/core";
     styleUrl: './header.component.css'
 })
 
-export class HeaderComponent {}
+export class HeaderComponent {
+    classes = ['close', 'minimize', 'maximize']
+}
