cff05c8994aa53f99e1af17dd594248527cb5c67
Denis00-ES <dstoynov2000@gmail.com>
2025-05-22 03:35:05 +0300

    add text on home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./components/navigation/navigation.component";
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavigationComponent]
+  imports: [RouterOutlet, NavigationComponent, NgIf]
 })
-export class AppComponent {}
+export class AppComponent {
+  private router = inject(Router);
+
+  isHomeRoute(): boolean {
+    return this.router.url === '/';
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HistoryManagerComponent } from './components/manager/manager.component'
 import { MainCalculatorComponent } from './components/calculator/main/main.component';
 
 export const routes: Routes = [
-    {path: 'calculator', component: MainCalculatorComponent},
+    { path: 'calculator', component: MainCalculatorComponent },
     { path: 'history-manager', component: HistoryManagerComponent }
 ];
